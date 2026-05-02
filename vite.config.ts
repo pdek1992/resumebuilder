4105c15b43a9be190b3d8ba54829cbde4adc1448
pdek1991 <pdek1991@gmail.com>
2026-05-02 17:34:40 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/resume-builder/', // Add this line! Change it to match your exact GitHub repository name
 })
